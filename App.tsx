079886ecb4ee5aeca22059852c2c649ed38754d0
RohaanAwan <l226574@lhr.nu.edu.pk>
2025-07-10 15:31:44 +0500

    Added basic routing based on authentication.

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { store, persistor } from './components/redux/store';
@@ -15,18 +15,36 @@ import Signup from './screens/Signup';
 
 const Stack = createStackNavigator();
 
+const LoggedInFlow = () => {
+  return (
+    <Stack.Navigator initialRouteName='MasterScreen' screenOptions={{headerShown: false}}>
+      <Stack.Screen name="MasterScreen" component={MasterScreen} />
+      <Stack.Screen name="ChatScreen" component={ChatScreen} />
+    </Stack.Navigator>
+  );
+}
+
+const StartFlow = () => {
+  return (
+    <Stack.Navigator initialRouteName='StartScreen' screenOptions={{headerShown: false}}>
+        <Stack.Screen name="StartScreen" component={StartScreen} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+    </Stack.Navigator>
+  )
+}
+
+const NavigationDecider =() => {
+  const currentUser = useSelector(state => state.currentUser);
+  return currentUser.id != '0' ? <LoggedInFlow />: <StartFlow />
+}
+
 const App = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <NavigationContainer>
-          <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="StartScreen">
-            <Stack.Screen name="StartScreen" component={StartScreen} />
-            <Stack.Screen name="MasterScreen" component={MasterScreen} />
-            <Stack.Screen name="ChatScreen" component={ChatScreen} />
-            <Stack.Screen name="Login" component={Login} />
-            <Stack.Screen name="Signup" component={Signup} />
-          </Stack.Navigator>
+            <NavigationDecider />
         </NavigationContainer>
       </PersistGate>
     </Provider>
